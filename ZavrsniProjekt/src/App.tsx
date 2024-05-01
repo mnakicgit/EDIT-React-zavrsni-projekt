0b10346f1c54a0204dc40299c45789d2870187f1
mnakicgit <mnakic00@fesb.hr>
2024-05-01 20:25:50 +0200

    Dodani podaciZavrsni.json i PopisGradova.tsx

--- ZavrsniProjekt/src/App.tsx
+++ ZavrsniProjekt/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useContext, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import PopisGradova from "./assets/PopisGradova";
 import Container from "react-bootstrap/Container";
 import Navigacija from "./components/Navigacija"; //koristen react-bootstrap, odvojeno u zasebnu komponentu radi preglednosti
 import Pocetna from "./components/Pocetna";
