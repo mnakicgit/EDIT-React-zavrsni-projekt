d4190da007cd1d0463217a538e5342ae77fa9e81
mnakicgit <mnakic00@fesb.hr>
2024-05-02 17:20:49 +0200

    Komponenta PodaciKarticeVolontera pripojena KarticiVolontera

--- ZavrsniProjekt/src/components/KarticaVolontera.tsx
+++ ZavrsniProjekt/src/components/KarticaVolontera.tsx
@@ -1,8 +1,8 @@
 /* Odlučila sam sve informacije prikazati na kartici umjeto u modalu radi preglednosti, osobno više volim odmah vidjeti sve informacije */
 
-import PodaciKarticeVolontera from "./PodaciKarticeVolontera";
 import Card from "react-bootstrap/Card"; /* Image card - kopirano sa sluzbene stranice i modificirano */
 import Image from "react-bootstrap/Image";
+import ListGroup from "react-bootstrap/ListGroup";
 
 interface TipoviVolontera {
 	id: number;
@@ -17,23 +17,30 @@ interface KartVolProps {
 }
 
 function KarticaVolontera(props: KartVolProps) {
+	{
+		props.volonteri.map((volonter) => {
+			volonter.aktivnosti.sort();
+		});
+	}
+
 	console.log(props.volonteri);
 
 	return (
 		<>
-			<Card className="m-2" style={{ width: "18rem" }}>
-				<Image className=".card-img-top" src="./src/assets/2538100.jpg" style={{ objectFit: "scale-down" }} />
-				<Card.Body>
-					<Card.Header>Card Title</Card.Header>
-					<Card.Text className="p-2">Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
-					<Card.Text className="p-2">
-						{" "}
-						{props.volonteri.map((r) => (
-							<PodaciKarticeVolontera key={r.id} rez={r} />
-						))}{" "}
-					</Card.Text>
-				</Card.Body>
-			</Card>
+			{props.volonteri.map((volonter) => (
+				<Card className="m-2" style={{ width: "16rem" }} key={volonter.id}>
+					<Image className=".card-img-top" src="./src/assets/2538100.jpg" style={{ objectFit: "scale-down" }} />
+					<Card.Body className="p-0">
+						<Card.Header className="card-title w-100">{volonter.ime}</Card.Header>
+
+						<ListGroup className="list-group-flush">
+							<ListGroup.Item>{volonter.grad}</ListGroup.Item>
+							<ListGroup.Item>{volonter.aktivnosti.join(" • ")}</ListGroup.Item>
+							<ListGroup.Item>{volonter.email}</ListGroup.Item>
+						</ListGroup>
+					</Card.Body>
+				</Card>
+			))}
 		</>
 	);
 }
